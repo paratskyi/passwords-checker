0b7e036841a7b81429e3ccb37f7f1e6ca6aec4ec
Bohdan Paratskyi <bog4242@gmail.com>
2023-05-04 09:26:54 +0300

    Add one more case to spec

--- spec/valid_password_counter_spec.rb
+++ spec/valid_password_counter_spec.rb
@@ -6,10 +6,12 @@ RSpec.describe ValidPasswordCounter do
 
   let(:valid_password_line_1) { 'a 1-5: abcdj' }
   let(:valid_password_line_2) { 'b 3-6: bhhkkbbjjjb' }
-  let(:invalid_password_line) { 'c 2-4: asfalseiruqwo' }
+  let(:invalid_password_line_1) { 'c 2-4: asfalseiruqwo' }
+  let(:invalid_password_line_2) { 'd 2-4: adsfdaldsedirduqwo' }
 
   let(:password_lines) { [] }
   let(:valid_password_lines) { [valid_password_line_1, valid_password_line_2] }
+  let(:invalid_password_lines) { [invalid_password_line_1, invalid_password_line_2] }
 
   before do
     password_lines.each do |password_line|
@@ -26,17 +28,25 @@ RSpec.describe ValidPasswordCounter do
   end
 
   context 'without valid passwords' do
-    let(:password_lines) { [invalid_password_line] }
+    let(:password_lines) { [invalid_password_line_1] }
 
     it { expect(subject).to eq 0 }
   end
 
   context 'with valid and invalid passwords' do
     let(:password_lines) do
-      [valid_password_line_1, invalid_password_line, valid_password_line_2]
+      [valid_password_line_1, invalid_password_line_1, valid_password_line_2]
     end
 
     it { expect(subject).to eq valid_password_lines.count }
+
+    context 'when there are many invalid passwords' do
+      let(:password_lines) do
+        [valid_password_line_1, invalid_password_line_1, valid_password_line_2, invalid_password_line_2]
+      end
+
+      it { expect(subject).to eq valid_password_lines.count }
+    end
   end
 
   context 'with invalid file_path' do
